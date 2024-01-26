a34d9b8c59d602410a7c7ca7622650f48a1a5057
cmatan10 <cmatan10@gmail.com>
2024-01-26 11:36:30 +0200

    first commit

--- scripts/addresses.js
+++ scripts/addresses.js
@@ -1,11 +1,12 @@
-const entryPointAddress = '0xAB0c7996Fc220A0c4bBE7b8b4ecA7B73242179Ba';
-const accountFactoryAddress = "0xf7e49d8765cdFdDE0ecc493ae12aD23fc84684a1";
+const entryPointAddress = '';
+const accountFactoryAddress = "";
 
-const eoaPublicKey = '0x4e81816F61d60D6fc35777129e25a4e1749cF155';
-const eoaPrivateKey = '0x42e89dad1b129c2ddbb10d7e470dc6e767ec1a7b51b4285ebfcea53e7c237dd5';
-const simpleAccountAddress = '0x76337bf48b17629B2760F2986Bd300d04d83A4f6';
+const eoaPublicKey = '';
+const eoaPrivateKey = '';
+const simpleAccountAddress = '';
 
-const exampleContractAddress = '0x3B5309068CF5B22E54C49EcFaDc756c72E91Df58';
+const exampleContractAddress = '';
+
 
 module.exports = {
     entryPointAddress,
