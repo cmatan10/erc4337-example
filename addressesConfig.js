20252cd9d0a5a01f4ebb5bde604257e4ec9622d0
cmatan10 <cmatan10@gmail.com>
2024-02-05 00:39:32 +0200

    update

--- addressesConfig.js
+++ addressesConfig.js
@@ -1,13 +1,13 @@
-const entryPointAddress = '0x263E969A73af3D110519a36D1296C429092dCcEA';
-const accountFactoryAddress = '0x6aA33B57596F8da4A41031A0EF1A331EE1511777';
+const entryPointAddress = '';
+const accountFactoryAddress = '';
 
-const paymasterAddress = '0x5918B1154E0b889681c645d34e0AC9f84801aAFB';
+const paymasterAddress = '';
 
-const eoaPublicKey = '0xd54a0975B5c3D2978Bc59acFC01534C825e9eE29';
-const eoaPrivateKey = '0xe61b406cc14ef1203007693dcffee268bc6192ee93f016691d407d76e98f7c10';
-const simpleAccountAddress = '0x94cf2f42ab6d13c1c7ffc25f44d221a52f9bc9f7';
+const eoaPublicKey = '';
+const eoaPrivateKey = '';
+const simpleAccountAddress = '';
 
-const exampleContractAddress = '0xB873b5C3d234715451ce828c2B4772fC30d965fB';
+const exampleContractAddress = '';
 
 
 module.exports = {
